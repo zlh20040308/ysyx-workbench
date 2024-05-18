7101d246adff53c3cbefe4bd2c7b2b978a0cd665
tracer-ysyx <tracer@ysyx.org>
2024-05-18 14:52:44 +0800

    > sim RTL ysyx_22040000 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  14:52:44 up  6:24,  1 user,  load average: 0.76, 0.52, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -5,39 +5,59 @@
 
 vluint64_t sim_time = 0;
 VerilatedVcdC *m_trace;
-static TOP_NAME* dut;
+static TOP_NAME *dut;
+bool is_trace = false;
 
-void nvboard_bind_all_pins(TOP_NAME* top);
+void nvboard_bind_all_pins(TOP_NAME *top);
 
-static void single_cycle() {
-  dut->clk = 0; dut->eval();m_trace->dump(sim_time);sim_time++;
-  dut->clk = 1; dut->eval();m_trace->dump(sim_time);sim_time++;
+static void single_cycle()
+{
+  dut->clk = 0;
+  dut->eval();
+  if (is_trace)
+    m_trace->dump(sim_time);
+  sim_time++;
+  dut->clk = 1;
+  dut->eval();
+  if (is_trace)
+    m_trace->dump(sim_time);
+  sim_time++;
 }
 
-static void reset(int n) {
+static void reset(int n)
+{
   dut->rst = 1;
-  while (n -- > 0) single_cycle();
+  while (n-- > 0)
+    single_cycle();
   dut->rst = 0;
 }
 
-int main(int argc, char** argv, char** env) {
+int main(int argc, char **argv, char **env)
+{
+
   Verilated::commandArgs(argc, argv);
   dut = new TOP_NAME;
   nvboard_bind_all_pins(dut);
   nvboard_init();
-  Verilated::traceEverOn(true);
-  m_trace = new VerilatedVcdC;
-  dut->trace(m_trace, 5);
-  m_trace->open("waveform.vcd");
+  /* 不要修改以上的代码 */
+
+  if (is_trace)
+  {
+    Verilated::traceEverOn(true);
+    m_trace = new VerilatedVcdC;
+    dut->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
+  }
 
   reset(10);
 
-  while(sim_time<650) {
+  while (sim_time < 650)
+  {
     nvboard_update();
     single_cycle();
   }
-
-  m_trace->close();
+  if (is_trace)
+    m_trace->close();
   delete dut;
   nvboard_quit();
 }
