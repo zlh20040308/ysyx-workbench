f922e71d29f2828bd5785bd2624ee1221af0564e
tracer-ysyx <tracer@ysyx.org>
2024-05-20 20:07:45 +0800

    > compile NEMU 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  20:07:45 up  7:06,  1 user,  load average: 0.13, 0.18, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -32,6 +32,8 @@ static bool g_print_step = false;
 
 void device_update();
 
+extern bool traverse_watchpoints_and_show_changes();
+
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
