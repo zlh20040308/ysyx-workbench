75fbc0b2f2cba4ffb4ba700ae91eed3e597c11c9
tracer-ysyx <tracer@ysyx.org>
2024-05-24 23:25:45 +0800

    > sim RTL 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  23:25:45 up  1:09,  1 user,  load average: 0.13, 0.13, 0.20

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,70 +4,24 @@
 #include <verilated_vcd_c.h>
 #include <iostream>
 
-vluint64_t sim_time = 0;
-VerilatedVcdC *m_trace = nullptr;
-VerilatedContext *contextp = nullptr;
-static TOP_NAME *dut = nullptr;
-const bool is_trace = false;
-
+static TOP_NAME* dut;
 void nvboard_bind_all_pins(TOP_NAME *top);
-
-static void single_cycle()
-{
-  dut->clk = 0;
-  dut->eval();
-  if (is_trace)
-    m_trace->dump(sim_time);
-  sim_time++;
-  dut->clk = 1;
-  dut->eval();
-  if (is_trace)
-    m_trace->dump(sim_time);
-  sim_time++;
-}
-
-static void reset(int n)
-{
-  dut->rst = 1;
-  while (n-- > 0)
-    single_cycle();
-  dut->rst = 0;
-}
-
-void sim_init()
-{
-  // verilate init
-  dut = new TOP_NAME;
-  contextp = new VerilatedContext;
-  
-}
 
 int main(int argc, char **argv, char **env)
 {
-  // Verilated::traceEverOn(true);
-  Verilated::commandArgs(argc, argv);
-  // nvboard init
+  dut = new TOP_NAME;
   nvboard_bind_all_pins(dut);
   nvboard_init();
-  sim_init();
 
-  // m_trace = new VerilatedVcdC;
-  // dut->trace(m_trace, 5);
-  // m_trace->open("waveform.vcd");
 
-  reset(10);
-
-  int i = 0;
-
-  while (i--)
-  {
+  while(1) {
     nvboard_update();
-    std::cout<< 1 << std::endl;
-    single_cycle();
+    dut->clk = 0;
+    dut->eval();
+    dut->clk = 1;
+    dut->eval();
   }
 
-  if (is_trace)
-    m_trace->close();
   delete dut;
   nvboard_quit();
 }
