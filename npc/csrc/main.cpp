d8075df4cb84a8e78af2e5ebbfea1d67232b92b5
tracer-ysyx <tracer@ysyx.org>
2024-05-24 22:33:52 +0800

    > sim RTL 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  22:33:52 up 17 min,  1 user,  load average: 0.31, 0.29, 0.20

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -45,16 +45,14 @@ void sim_init()
 
 int main(int argc, char **argv, char **env)
 {
+  Verilated::traceEverOn(true);
   Verilated::commandArgs(argc, argv);
   sim_init();
-  if (is_trace)
-  {
-    Verilated::traceEverOn(true);
-    m_trace = new VerilatedVcdC;
-    dut->trace(m_trace, 5);
-    m_trace->open("waveform.vcd");
-  }
-  
+
+  m_trace = new VerilatedVcdC;
+  dut->trace(m_trace, 5);
+  m_trace->open("waveform.vcd");
+
   reset(10);
 
   while (5)
