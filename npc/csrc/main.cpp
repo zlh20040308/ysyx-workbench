3f0cbc375597422b2cce5dc5f4edc77dc7d65751
tracer-ysyx <tracer@ysyx.org>
2024-05-25 09:55:04 +0800

    > sim RTL 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:55:04 up  1:18,  1 user,  load average: 0.27, 0.34, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,27 +1,69 @@
-#include <nvboard.h>
-#include <Vtop.h>
-#include <verilated.h>
-#include <verilated_vcd_c.h>
-#include <iostream>
+// #include <nvboard.h>
+// #include <Vtop.h>
+// #include <verilated.h>
+// #include <verilated_vcd_c.h>
+// #include <iostream>
 
-static TOP_NAME* dut;
-void nvboard_bind_all_pins(TOP_NAME *top);
+// static TOP_NAME* dut;
+// void nvboard_bind_all_pins(TOP_NAME *top);
 
-int main(int argc, char **argv, char **env)
-{
-  dut = new TOP_NAME;
-  nvboard_bind_all_pins(dut);
-  nvboard_init();
+// int main(int argc, char **argv, char **env)
+// {
+//   dut = new TOP_NAME;
+//   nvboard_bind_all_pins(dut);
+//   nvboard_init();
 
 
-  while(1) {
-    nvboard_update();
-    dut->clk = 0;
-    dut->eval();
-    dut->clk = 1;
-    dut->eval();
-  }
+//   while(1) {
+//     nvboard_update();
+//     dut->clk = 0;
+//     dut->eval();
+//     dut->clk = 1;
+//     dut->eval();
+//   }
 
-  delete dut;
-  nvboard_quit();
+//   delete dut;
+//   nvboard_quit();
+// }
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vmux21.h"
+
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static Vmux21* top;
+
+void step_and_dump_wave(){
+  top->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+}
+void sim_init(){
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new Vmux21;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 0);
+  tfp->open("dump.vcd");
+}
+
+void sim_exit(){
+  step_and_dump_wave();
+  tfp->close();
+}
+
+int main() {
+  sim_init();
+
+  top->s=0; top->a=0; top->b=0;  step_and_dump_wave();   // 将s，a和b均初始化为“0”
+                      top->b=1;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，继续保持“0”，
+            top->a=1; top->b=0;  step_and_dump_wave();   // 将a，b分别改为“1”和“0”，s的值不变，
+                      top->b=1;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，维持10个时间单位
+  top->s=1; top->a=0; top->b=0;  step_and_dump_wave();   // 将s，a，b分别变为“1,0,0”，维持10个时间单位
+                      top->b=1;  step_and_dump_wave();
+            top->a=1; top->b=0;  step_and_dump_wave();
+                      top->b=1;  step_and_dump_wave();
+
+  sim_exit();
 }
