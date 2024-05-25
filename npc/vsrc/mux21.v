f5e6527d8769b9b0c27c8a0672957632b512f382
tracer-ysyx <tracer@ysyx.org>
2024-05-25 10:37:36 +0800

    > sim RTL 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  10:37:36 up  1:40,  1 user,  load average: 0.09, 0.17, 0.20

--- npc/vsrc/mux21.v
+++ npc/vsrc/mux21.v
@@ -1,7 +1,32 @@
 module mux21(a,b,s,y);
-  input   a,b,s;        // 声明3个wire型输入变量a,b,和s，其宽度为1位。
-  output  y;           // 声明1个wire型输出变量y，其宽度为1位。
+  input  a,b,s;
+  output y;
 
-  assign  y = (~s&a)|(s&b);  // 实现电路的逻辑功能。
-
+  wire l, r, s_n; // 内部网线声明
+  my_not i1(.a(s), .b(s_n));        // 实例化非门，实现~s
+  my_and i2(.a(s_n), .b(a), .c(l)); // 实例化与门，实现(~s&a)
+  my_and i3(.a(s),   .b(b), .c(r)); // 实例化与门，实现(s&b)
+  my_or  i4(.a(l),   .b(r), .c(y)); // 实例化或门，实现(~s&a)|(s&b)
 endmodule
+
+module my_and(a,b,c);
+  input  a,b;
+  output c;
+
+  assign c = a & b;
+endmodule
+
+module my_or(a,b,c);
+  input  a,b;
+  output c;
+
+  assign c = a | b;
+endmodule
+
+module my_not(a,b);
+  input  a;
+  output b;
+
+  assign b = ~a;
+endmodule
+
