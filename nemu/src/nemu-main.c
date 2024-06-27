6965a73995234f86aff720f4f8a3e607472c6642
tracer-ysyx <tracer@ysyx.org>
2024-06-27 15:53:10 +0800

    > compile NEMU 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:53:10 up  9:23,  4 users,  load average: 0.39, 0.34, 0.24

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <common.h>
 
@@ -19,8 +19,55 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+word_t expr(char *e, bool *success);
 
-int main(int argc, char *argv[]) {
+#define BUF_SIZE 65536
+
+// 读取文件内容并处理每一行
+void process_file(const char *filename)
+{
+  FILE *file = fopen(filename, "r");
+  if (!file)
+  {
+    perror("Failed to open file");
+    exit(EXIT_FAILURE);
+  }
+
+  char line[BUF_SIZE];
+  while (fgets(line, sizeof(line), file))
+  {
+    unsigned expected_result;
+    char expression[BUF_SIZE];
+
+    // 解析行格式 "结果 表达式"
+    if (sscanf(line, "%u %[^\n]", &expected_result, expression) != 2)
+    {
+      fprintf(stderr, "Failed to parse line: %s", line);
+      continue;
+    }
+
+    bool success = false;
+    word_t actual_result = expr(expression, &success);
+
+    if (success)
+    {
+      printf("Expected: %u, Actual: %u, Expression: %s\n", expected_result, actual_result, expression);
+      if (expected_result != actual_result)
+      {
+        printf("Mismatch detected!\n");
+      }
+    }
+    else
+    {
+      printf("Expression parsing failed: %s\n", expression);
+    }
+  }
+
+  fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
@@ -29,7 +76,9 @@ int main(int argc, char *argv[]) {
 #endif
 
   /* Start engine. */
-  engine_start();
+  // engine_start();
+  const char *filename = "/home/feng/OS/ysyx-workbench/nemu/tools/gen-expr/input";
+  process_file(filename);
 
   return is_exit_status_bad();
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -20,6 +20,9 @@
 #include <assert.h>
 #include <string.h>
 
+
+#define BUF_SIZE 65536
+
 // this should be enough
 static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
@@ -32,81 +35,79 @@ static char *code_format =
     "}";
 
 static int buf_index = 0;
+static int max_depth = 10; // Maximum depth of the expression
 
-typedef enum
-{
-  OP_ADD,      // 加法 "+"
-  OP_SUBTRACT, // 减法 "-"
-  OP_MULTIPLY, // 乘法 "*"
-  OP_DIVIDE,   // 除法 "/"
-  OP_EQ,       // 相等 "=="
-  OP_NOTEQ,    // 不等 "!="
-  OP_LAND,     // 逻辑与 "&&"
-  OP_COUNT     // 操作符计数（用于限制范围）
+typedef enum {
+    OP_ADD,       // 加法 "+"
+    OP_SUBTRACT,  // 减法 "-"
+    OP_MULTIPLY,  // 乘法 "*"
+    OP_DIVIDE,    // 除法 "/"
+    OP_EQ,        // 相等 "=="
+    OP_NOTEQ,     // 不等 "!="
+    OP_LAND,      // 逻辑与 "&&"
+    OP_COUNT      // 操作符计数（用于限制范围）
 } Op;
 
-// 返回操作符对应的字符
-const char *op_to_string(Op op)
-{
-  switch (op)
-  {
-  case OP_ADD:
-    return "+";
-  case OP_SUBTRACT:
-    return "-";
-  case OP_MULTIPLY:
-    return "*";
-  case OP_DIVIDE:
-    return "/";
-  case OP_EQ:
-    return "==";
-  case OP_NOTEQ:
-    return "!=";
-  case OP_LAND:
-    return "&&";
-  default:
-    assert(0);
-    return "";
-  }
+// Return the string representation of the operator
+const char* op_to_string(Op op) {
+    switch (op) {
+        case OP_ADD: return "+";
+        case OP_SUBTRACT: return "-";
+        case OP_MULTIPLY: return "*";
+        case OP_DIVIDE: return "/";
+        case OP_EQ: return "==";
+        case OP_NOTEQ: return "!=";
+        case OP_LAND: return "&&";
+        default: assert(0); return "";
+    }
 }
 
-uint32_t choose(uint32_t n)
-{
-  return rand() % n;
+uint32_t choose(uint32_t n) {
+    return rand() % n;
 }
 
-static void gen_rand_op()
-{
-  uint32_t op_idx = choose(OP_COUNT);
-  const char *op = op_to_string((Op)op_idx);
-  for (int i = 0; op[i] != '\0'; i++)
-  {
-    buf[buf_index++] = op[i];
-  }
+static void gen_rand_op() {
+    uint32_t op_idx = choose(OP_COUNT);
+    const char* op = op_to_string((Op)op_idx);
+    // Randomly insert spaces around the operator
+    if (choose(2) == 0) {
+        buf[buf_index++] = ' ';
+    }
+    for (int i = 0; op[i] != '\0'; i++) {
+        buf[buf_index++] = op[i];
+    }
+    if (choose(2) == 0) {
+        buf[buf_index++] = ' ';
+    }
 }
 
-static void gen_rand_expr()
-{
-  switch (choose(3))
-  {
-  case 0:
-  {
-    int num = choose(100);
-    buf_index += sprintf(buf + buf_index, "%d", num);
-    break;
-  }
-  case 1:
-    buf[buf_index++] = '(';
-    gen_rand_expr();
-    buf[buf_index++] = ')';
-    break;
-  default:
-    gen_rand_expr();
-    gen_rand_op();
-    gen_rand_expr();
-    break;
-  }
-  buf[buf_index] = '\0';
+static void gen_rand_expr(int depth) {
+    if (depth > max_depth || buf_index >= BUF_SIZE - 32) {
+        // If the depth is too high or buffer is nearly full, generate a number
+        int num = choose(100);
+        buf_index += sprintf(buf + buf_index, "%u", num);
+        return;
+    }
+    switch (choose(3)) {
+    case 0: {
+        // Generate a random number
+        int num = choose(100);
+        buf_index += sprintf(buf + buf_index, "%u", num);
+        break;
+    }
+    case 1:
+        buf[buf_index++] = '(';
+        gen_rand_expr(depth + 1);
+        buf[buf_index++] = ')';
+        break;
+    case 2: {
+        gen_rand_expr(depth + 1);
+        gen_rand_op();
+        gen_rand_expr(depth + 1);
+        break;
+    }
+    }
+    buf[buf_index] = '\0';
 }
 
 int main(int argc, char *argv[])
@@ -122,7 +123,7 @@ int main(int argc, char *argv[])
   for (i = 0; i < loop; i++)
   {
     buf_index = 0; // 在每次生成表达式前重置 buf_index
-    gen_rand_expr();
+    gen_rand_expr(0);
 
     sprintf(code_buf, code_format, buf);
 
