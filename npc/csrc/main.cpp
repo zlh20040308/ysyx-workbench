7167792a768caad92994a2fb116a49c439093343
tracer-ysyx <tracer@ysyx.org>
2024-07-07 12:06:37 +0800

    > compile NEMU 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  12:06:37 up  2:34,  1 user,  load average: 0.10, 0.15, 0.16

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,62 +1,76 @@
-#include <verilated.h>
-#include "Vtop.h"
-#include <nvboard.h>
+// #include <verilated.h>
+// #include "Vtop.h"
+// #include <nvboard.h>
 
-static TOP_NAME* dut;
-void nvboard_bind_all_pins(TOP_NAME* top);
+// static TOP_NAME* dut;
+// void nvboard_bind_all_pins(TOP_NAME* top);
+
+// int main()
+// {
+//   dut = new TOP_NAME;
+//   nvboard_bind_all_pins(dut);
+//   nvboard_init();
+//   while (1)
+//   {
+//     dut->eval();
+//     nvboard_update();
+//   }
+
+//   delete dut;
+//   nvboard_quit();
+// }
+
+uint32_t M[] = {
+  
+}
+
+
+#include <stdint.h>
+#include "verilated_vcd_c.h"
+#include "Vysyx_23060337_top.h"
+
+VerilatedContext *contextp = NULL;
+VerilatedVcdC *tfp = NULL;
+
+static Vysyx_23060337_top *top;
+
+void step_and_dump_wave()
+{
+  top->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+}
+void sim_init()
+{
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new Vysyx_23060337_top;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 0);
+  tfp->open("waveform.vcd");
+}
+
+void sim_exit()
+{
+  step_and_dump_wave();
+  tfp->close();
+}
+
+
+uint32_t pmem_read(uint32_t pc){
+
+}
 
 int main()
 {
-  dut = new TOP_NAME;
-  nvboard_bind_all_pins(dut);
-  nvboard_init();
-  while (1)
+  sim_init();
+
+  while (???)
   {
-    dut->eval();
-    nvboard_update();
+
+    top->inst = pmem_read(top->pc);
+    top->eval();
   }
 
-  delete dut;
-  nvboard_quit();
+  sim_exit();
 }
-
-// #include "verilated_vcd_c.h"
-// #include "Vdecode24.h"
-
-// VerilatedContext* contextp = NULL;
-// VerilatedVcdC* tfp = NULL;
-
-// static Vdecode24* top;
-
-// void step_and_dump_wave(){
-//   top->eval();
-//   contextp->timeInc(1);
-//   tfp->dump(contextp->time());
-// }
-// void sim_init(){
-//   contextp = new VerilatedContext;
-//   tfp = new VerilatedVcdC;
-//   top = new Vdecode24;
-//   contextp->traceEverOn(true);
-//   top->trace(tfp, 0);
-//   tfp->open("waveform.vcd");
-// }
-
-// void sim_exit(){
-//   step_and_dump_wave();
-//   tfp->close();
-// }
-
-// int main() {
-//   sim_init();
-
-//   top->en = 0b0;  top->x = 0b00;  step_and_dump_wave();
-//                   top->x = 0b01;  step_and_dump_wave();
-//                   top->x = 0b10;  step_and_dump_wave();
-//                   top->x = 0b11;  step_and_dump_wave();
-//   top->en = 0b1;  top->x = 0b00;  step_and_dump_wave();
-//                   top->x = 0b01;  step_and_dump_wave();
-//                   top->x = 0b10;  step_and_dump_wave();
-//                   top->x = 0b11;  step_and_dump_wave();
-//   sim_exit();
-// }
--- npc/vsrc/ysyx_23060337_top.v
+++ npc/vsrc/ysyx_23060337_top.v
@@ -1,29 +1,10 @@
 module ysyx_23060337_top(
     input clk,
     input rst,
-    input [4:0] btn,
-    input [15:0] sw,
-    input ps2_clk,
-    input ps2_data,
-    input uart_rx,
-    output uart_tx,
-    output [15:0] ledr,
-    output VGA_CLK,
-    output VGA_HSYNC,
-    output VGA_VSYNC,
-    output VGA_BLANK_N,
-    output [7:0] VGA_R,
-    output [7:0] VGA_G,
-    output [7:0] VGA_B,
-    output [7:0] seg0,
-    output [7:0] seg1,
-    output [7:0] seg2,
-    output [7:0] seg3,
-    output [7:0] seg4,
-    output [7:0] seg5,
-    output [7:0] seg6,
-    output [7:0] seg7
+    input [31:0] inst,
+    output reg [31:0] pc,
 );
+
 
 
 
