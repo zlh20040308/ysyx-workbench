efd452bbd5e5a8bc7f0a949853078d361f5dac97
tracer-ysyx <tracer@ysyx.org>
2024-07-10 15:03:21 +0800

    > sim RTL 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:03:21 up 1 day, 16:16,  1 user,  load average: 0.20, 0.33, 0.29

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -112,55 +112,68 @@ int main()
   top->rst = 1;
 
   top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
   top->clk = 1;
-  top->eval();
+  step_and_dump_wave();
   top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 1;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 1;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
 
   top->rst = 0;
-  top->eval();
+  step_and_dump_wave();
 
   top->clk = 1;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 0;
-  top->eval();
-  top->clk = 1;
-  top->eval();
-  top->clk = 0;
-  top->eval();
-  top->clk = 1;
-  top->eval();
-  top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
 
   top->clk = 1;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
 
   top->clk = 1;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
 
   top->clk = 1;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
 
   top->clk = 1;
-  top->eval();
+  step_and_dump_wave();
+
   top->clk = 0;
-  top->eval();
+  step_and_dump_wave();
+
+  top->clk = 1;
+  step_and_dump_wave();
+
+  top->clk = 0;
+  step_and_dump_wave();
+
+  top->clk = 1;
+  step_and_dump_wave();
+
+  top->clk = 0;
+  step_and_dump_wave();
 
   sim_exit();
 }
