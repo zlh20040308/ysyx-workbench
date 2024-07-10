fe34a3a099dca570a998b3d09b8b946408039d85
tracer-ysyx <tracer@ysyx.org>
2024-07-10 14:45:16 +0800

    > sim RTL 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  14:45:16 up 1 day, 15:58,  1 user,  load average: 0.28, 0.30, 0.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -95,11 +95,9 @@ void sim_exit()
 
 extern "C" uint32_t pmem_read(uint32_t raddr)
 {
-  printf("ahaha\n");
   if ((raddr - MEM_BASE) <= 36)
   { // 这里没有必要比较 `>=0`，因为是无符号整数
-    printf("%u\n", *(uint32_t *)(pmem + (raddr - MEM_BASE)));
-    printf("haha\n");
+    printf("inst %u\n", *(uint32_t *)(pmem + (raddr - MEM_BASE)));
     return *(uint32_t *)(pmem + (raddr - MEM_BASE));
   }
 
