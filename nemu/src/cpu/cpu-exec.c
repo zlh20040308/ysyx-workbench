f6d1b393e9d1c364ddd6848aef152e46a55285c5
tracer-ysyx <tracer@ysyx.org>
2024-07-13 10:52:48 +0800

    > compile NEMU 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  10:52:48 up 3 days,  8:46,  1 user,  load average: 0.20, 0.18, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -90,7 +90,7 @@ static void exec_once(Decode *s, vaddr_t pc)
 
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
               MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
-  Log("%s", s->logbuf);
+  // Log("%s", s->logbuf);
 
 #else
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
@@ -105,7 +105,9 @@ static void execute(uint64_t n)
   {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst++;
+    Log("hhhhhhhhhhh");
     trace_and_difftest(&s, cpu.pc);
+
     if (nemu_state.state != NEMU_RUNNING)
       break;
     IFDEF(CONFIG_DEVICE, device_update());
