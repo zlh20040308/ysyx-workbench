a1a6fac434f88bac292f4ebd32128f815170f2ef
tracer-ysyx <tracer@ysyx.org>
2024-07-13 10:46:51 +0800

    > compile NEMU 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  10:46:51 up 3 days,  8:40,  1 user,  load average: 0.17, 0.18, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -56,24 +56,25 @@ static void exec_once(Decode *s, vaddr_t pc) {
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
-  Log("%s",p);
+  char *log_p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-  Log("%s",p);
+    Log("%s",log_p);
+
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
-    Log("%s",p);
+    Log("%s",log_p);
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
-  Log("%s",p);
   p += space_len;
-  Log("%s",p);
+    Log("%s",log_p);
+
 
 #ifndef CONFIG_ISA_loongarch32r
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
