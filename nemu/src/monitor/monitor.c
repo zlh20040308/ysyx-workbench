b74ce541d288fb10eee3ff7f4c61c9fb01f6102e
tracer-ysyx <tracer@ysyx.org>
2024-07-14 10:53:55 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  10:53:55 up  2:21,  1 user,  load average: 0.20, 0.24, 0.29

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -160,6 +160,9 @@ static long parse_elf()
   // const Elf32_Phdr *program_header = elf + elf_header->e_phoff;
   const Elf32_Shdr *section_table = elf + elf_header->e_shoff;
   printf("Name %s\n",  (char *)(elf + elf_header->e_shoff + section_table[1].sh_name));
+  printf("Name %s\n",  (char *)(elf + section_table[1].sh_name));
+  printf("Name %s\n",  (char *)(elf + elf_header->e_shoff + section_table[1].sh_name));
+
   printf("Name %x\n",  section_table[2].sh_name);
 
 
