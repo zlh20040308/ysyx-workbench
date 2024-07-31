fe169a968f498b1ba3fc91ba493c2d0f59596aa1
tracer-ysyx <tracer@ysyx.org>
2024-07-31 10:35:39 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  10:35:39 up  6:40,  1 user,  load average: 1.05, 0.96, 0.65

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,50 +1,47 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
+#define BUFFER_SIZE 1024
+static char buf[BUFFER_SIZE];
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...)
-{
-  panic("Not implemented");
+int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsnprintf(buf, BUFFER_SIZE, fmt, ap);
+  va_end(ap);
+  putstr(buf);
+  return ret;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap)
-{
+int vsprintf(char *out, const char *fmt, va_list ap) {
   int format = 0;
   int longarg = 0;
   size_t pos = 0;
-  for (; *fmt; fmt++)
-  {
-    if (format)
-    {
-      switch (*fmt)
-      {
-      case '%':
-      {
+  for (; *fmt; fmt++) {
+    if (format) {
+      switch (*fmt) {
+      case '%': {
         out[pos++] = '%';
         longarg = 0;
         format = 0;
         break;
       }
-      case 'l':
-      {
+      case 'l': {
         ++longarg;
         break;
       }
-      case 'p':
-      {
+      case 'p': {
         ++longarg;
         out[pos++] = '0';
         out[pos++] = 'x';
       }
-      case 'x':
-      {
+      case 'x': {
         long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
         int hexdigits = 2 * (longarg ? sizeof(long) : sizeof(int)) - 1;
-        for (int i = hexdigits; i >= 0; i--)
-        {
+        for (int i = hexdigits; i >= 0; i--) {
           int d = (num >> (4 * i)) & 0xF;
           out[pos] = (d < 10 ? '0' + d : 'a' + d - 10);
           pos++;
@@ -53,11 +50,9 @@ int vsprintf(char *out, const char *fmt, va_list ap)
         format = 0;
         break;
       }
-      case 'd':
-      {
+      case 'd': {
         long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
-        if (num < 0)
-        {
+        if (num < 0) {
           num = -num;
           out[pos] = '-';
           pos++;
@@ -65,8 +60,7 @@ int vsprintf(char *out, const char *fmt, va_list ap)
         long digits = 1;
         for (long nn = num; nn /= 10; digits++)
           ;
-        for (int i = digits - 1; i >= 0; i--)
-        {
+        for (int i = digits - 1; i >= 0; i--) {
           out[pos + i] = '0' + (num % 10);
           num /= 10;
         }
@@ -75,11 +69,9 @@ int vsprintf(char *out, const char *fmt, va_list ap)
         format = 0;
         break;
       }
-      case 's':
-      {
+      case 's': {
         const char *s2 = va_arg(ap, const char *);
-        while (*s2)
-        {
+        while (*s2) {
           out[pos++] = *s2;
           s2++;
         }
@@ -87,8 +79,7 @@ int vsprintf(char *out, const char *fmt, va_list ap)
         format = 0;
         break;
       }
-      case 'c':
-      {
+      case 'c': {
         out[pos++] = (char)va_arg(ap, int);
         longarg = 0;
         format = 0;
@@ -97,13 +88,9 @@ int vsprintf(char *out, const char *fmt, va_list ap)
       default:
         break;
       }
-    }
-    else if (*fmt == '%')
-    {
+    } else if (*fmt == '%') {
       format = 1;
-    }
-    else
-    {
+    } else {
       out[pos++] = *fmt;
     }
   }
@@ -111,8 +98,7 @@ int vsprintf(char *out, const char *fmt, va_list ap)
   return pos;
 }
 
-int sprintf(char *out, const char *fmt, ...)
-{
+int sprintf(char *out, const char *fmt, ...) {
   int res = 0;
   va_list ap;
   va_start(ap, fmt);
@@ -121,14 +107,89 @@ int sprintf(char *out, const char *fmt, ...)
   return res;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...)
-{
-  panic("Not implemented");
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  int res = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  res = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return res;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
-{
-  panic("Not implemented");
+/*
+  %d
+  %s
+  %c
+ */
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  int format = 0;
+  size_t pos = 0;
+  size_t count = 0;
+  for (; *fmt; ++fmt) {
+    if (format) {
+      switch (*fmt) {
+      case 'c': {
+        if (pos < n) {
+          out[pos++] = (char)va_arg(ap, int);
+        }
+        ++count;
+        format = 0;
+        break;
+      }
+      case 's': {
+        const char *s2 = va_arg(ap, const char *);
+        while (*s2) {
+          if (pos < n) {
+            out[pos++] = *s2;
+            s2++;
+          }
+          ++count;
+        }
+        format = 0;
+        break;
+      }
+      case 'd': {
+        int num = va_arg(ap, int);
+        if (num < 0) {
+          num = -num;
+          if (pos < n) {
+            out[pos++] = '-';
+          }
+          ++count;
+        }
+        long digits = 1;
+        for (long nn = num; nn /= 10; digits++)
+          ;
+        for (int i = digits - 1; i >= 0; i--) {
+          if (pos + i < n) {
+            out[pos + i] = '0' + (num % 10);
+          }
+          num /= 10;
+        }
+        pos += digits;
+        count += digits;
+        format = 0;
+        break;
+      }
+      default:
+        break;
+      }
+    } else if (*fmt == '%') {
+      format = 1;
+    } else {
+      if (pos < n) {
+        out[pos++] = *fmt;
+      }
+      ++count;
+    }
+  }
+
+  if (pos < n) {
+    out[pos] = '\0';
+  }
+  out[n - 1] = '\0';
+  return count;
 }
 
 #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,127 +1,101 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s)
-{
+size_t strlen(const char *s) {
   size_t i = 0;
   while (s[i] != '\0')
     ++i;
   return i;
 }
 
-char *strcpy(char *dst, const char *src)
-{
+char *strcpy(char *dst, const char *src) {
   size_t i = 0;
-  for (; src[i] != '\0'; i++)
-  {
+  for (; src[i] != '\0'; i++) {
     dst[i] = src[i];
   }
   dst[i] = '\0';
   return dst;
 }
 
-char *strncpy(char *dst, const char *src, size_t n)
-{
+char *strncpy(char *dst, const char *src, size_t n) {
   size_t i = 0;
-  for (; src[i] != '\0' && i < n; i++)
-  {
+  for (; src[i] != '\0' && i < n; i++) {
     dst[i] = src[i];
   }
   dst[i++] = '\0';
-  for (; i < n; i++)
-  {
+  for (; i < n; i++) {
     dst[i] = '\0';
   }
   return dst;
 }
 
-char *strcat(char *dst, const char *src)
-{
+char *strcat(char *dst, const char *src) {
   size_t dst_len = strlen(dst);
   size_t src_len = strlen(src);
   size_t i = 0;
-  for (; i < src_len; i++)
-  {
+  for (; i < src_len; i++) {
     dst[dst_len + i] = src[i];
   }
   dst[dst_len + i] = '\0';
   return dst;
 }
 
-int strcmp(const char *s1, const char *s2)
-{
-  for (size_t i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-  {
-    if (s1[i] < s2[i])
-    {
+int strcmp(const char *s1, const char *s2) {
+  for (size_t i = 0; s1[i] != '\0' && s2[i] != '\0'; i++) {
+    if (s1[i] < s2[i]) {
       return -1;
     }
-    if (s1[i] > s2[i])
-    {
+    if (s1[i] > s2[i]) {
       return 1;
     }
   }
   size_t s1_len = strlen(s1);
   size_t s2_len = strlen(s2);
-  if (s1_len < s2_len)
-  {
+  if (s1_len < s2_len) {
     return -1;
   }
-  if (s1_len > s2_len)
-  {
+  if (s1_len > s2_len) {
     return 1;
   }
 
   return 0;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n)
-{
-  for (size_t i = 0; i < n; i++)
-  {
-    if (s1[i] < s2[i])
-    {
+int strncmp(const char *s1, const char *s2, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    if (s1[i] < s2[i]) {
       return -1;
     }
-    if (s1[i] > s2[i])
-    {
+    if (s1[i] > s2[i]) {
       return 1;
     }
   }
   return 0;
 }
 
-void *memset(void *s, int c, size_t n)
-{
+void *memset(void *s, int c, size_t n) {
   unsigned char *p = s;
-  while (n--)
-  {
+  while (n--) {
     *p++ = (unsigned char)c;
   }
   return s;
 }
 
-void *memmove(void *dst, const void *src, size_t n)
-{
+void *memmove(void *dst, const void *src, size_t n) {
   unsigned char *d = dst;
   const unsigned char *s = src;
 
-  if (d < s)
-  {
+  if (d < s) {
     // 如果 dst 在 src 之前，直接从前往后复制
-    for (size_t i = 0; i < n; i++)
-    {
+    for (size_t i = 0; i < n; i++) {
       d[i] = s[i];
     }
-  }
-  else
-  {
+  } else {
     // 如果 dst 在 src 之后，反向复制以避免重叠
-    for (size_t i = n; i > 0; i--)
-    {
+    for (size_t i = n; i > 0; i--) {
       d[i - 1] = s[i - 1];
     }
   }
@@ -129,31 +103,25 @@ void *memmove(void *dst, const void *src, size_t n)
   return dst;
 }
 
-void *memcpy(void *out, const void *in, size_t n)
-{
+void *memcpy(void *out, const void *in, size_t n) {
   unsigned char *out_ptr = out;
   const unsigned char *in_ptr = in;
 
-  for (size_t i = 0; i < n; i++)
-  {
+  for (size_t i = 0; i < n; i++) {
     out_ptr[i] = in_ptr[i];
   }
   return out;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n)
-{
+int memcmp(const void *s1, const void *s2, size_t n) {
 
   const unsigned char *s1_ptr = s1;
   const unsigned char *s2_ptr = s2;
-  for (size_t i = 0; i < n; i++)
-  {
-    if (s1_ptr[i] < s2_ptr[i])
-    {
+  for (size_t i = 0; i < n; i++) {
+    if (s1_ptr[i] < s2_ptr[i]) {
       return -1;
     }
-    if (s1_ptr[i] > s2_ptr[i])
-    {
+    if (s1_ptr[i] > s2_ptr[i]) {
       return 1;
     }
   }
