b4f709a461ab286f194026aa63130e76b134108a
tracer-ysyx <tracer@ysyx.org>
2024-08-10 15:54:59 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:54:59 up 2 days, 21:50,  1 user,  load average: 0.46, 0.39, 0.31

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -2,21 +2,21 @@
  * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
  *
  * NEMU is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
  *          http://license.coscl.org.cn/MulanPSL2
  *
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
  *
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
 
-#include <memory/host.h>
-#include <memory/paddr.h>
 #include <device/mmio.h>
 #include <isa.h>
+#include <memory/host.h>
+#include <memory/paddr.h>
 
 #if defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
@@ -27,48 +27,39 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 uint8_t *guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
-static word_t pmem_read(paddr_t addr, int len)
-{
+static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
 #ifdef CONFIG_MTRACE_COND
-  if (MTRACE_COND)
-  {
-    Log(FMT_WORD" "FMT_WORD, addr, ret);
-  }
+  Log(FMT_WORD " " FMT_WORD, addr, ret);
 #endif
   return ret;
 }
 
-static void pmem_write(paddr_t addr, int len, word_t data)
-{
+static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
 #ifdef CONFIG_MTRACE_COND
-  if (MTRACE_COND)
-  {
-    Log(FMT_WORD" "FMT_WORD, addr, data);
-  }
+  Log(FMT_WORD " " FMT_WORD, addr, data);
 #endif
 }
 
-static void out_of_bound(paddr_t addr)
-{
-  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+static void out_of_bound(paddr_t addr) {
+  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR
+        ", " FMT_PADDR "] at pc = " FMT_WORD,
         addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
 
-void init_mem()
-{
+void init_mem() {
 #if defined(CONFIG_PMEM_MALLOC)
   pmem = malloc(CONFIG_MSIZE);
   assert(pmem);
 #endif
   // 如果设置了 CONFIG_MEM_RANDOM ，就将 pmem 的所有 元素 随机初始化
   IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
-  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
+  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT,
+      PMEM_RIGHT);
 }
 
-word_t paddr_read(paddr_t addr, int len)
-{
+word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr)))
     return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -76,13 +67,11 @@ word_t paddr_read(paddr_t addr, int len)
   return 0;
 }
 
-void paddr_write(paddr_t addr, int len, word_t data)
-{
-  if (likely(in_pmem(addr)))
-  {
+void paddr_write(paddr_t addr, int len, word_t data) {
+  if (likely(in_pmem(addr))) {
     pmem_write(addr, len, data);
     return;
   }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return );
   out_of_bound(addr);
 }
