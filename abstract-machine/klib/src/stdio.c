0ca184d0aa1e34534521ac66a41f56f01b2814d3
tracer-ysyx <tracer@ysyx.org>
2024-08-12 20:15:28 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  20:15:28 up  3:47,  1 user,  load average: 0.31, 0.33, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,10 +2,12 @@
 #include <klib-macros.h>
 #include <klib.h>
 #include <stdarg.h>
+
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
 #define BUFFER_SIZE 1024
 static char buf[BUFFER_SIZE];
 
-#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
   va_list ap;
@@ -16,86 +18,93 @@ int printf(const char *fmt, ...) {
   return ret;
 }
 
+// int vsprintf(char *out, const char *fmt, va_list ap) {
+//   int format = 0;
+//   int longarg = 0;
+//   size_t pos = 0;
+//   for (; *fmt; fmt++) {
+//     if (format) {
+//       switch (*fmt) {
+//       case '%': {
+//         out[pos++] = '%';
+//         longarg = 0;
+//         format = 0;
+//         break;
+//       }
+//       case 'l': {
+//         ++longarg;
+//         break;
+//       }
+//       case 'p': {
+//         ++longarg;
+//         out[pos++] = '0';
+//         out[pos++] = 'x';
+//       }
+//       case 'x': {
+//         long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
+//         int hexdigits = 2 * (longarg ? sizeof(long) : sizeof(int)) - 1;
+//         for (int i = hexdigits; i >= 0; i--) {
+//           int d = (num >> (4 * i)) & 0xF;
+//           out[pos] = (d < 10 ? '0' + d : 'a' + d - 10);
+//           pos++;
+//         }
+//         longarg = 0;
+//         format = 0;
+//         break;
+//       }
+//       case 'd': {
+//         long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
+//         if (num < 0) {
+//           num = -num;
+//           out[pos] = '-';
+//           pos++;
+//         }
+//         long digits = 1;
+//         for (long nn = num; nn /= 10; digits++)
+//           ;
+//         for (int i = digits - 1; i >= 0; i--) {
+//           out[pos + i] = '0' + (num % 10);
+//           num /= 10;
+//         }
+//         pos += digits;
+//         longarg = 0;
+//         format = 0;
+//         break;
+//       }
+//       case 's': {
+//         const char *s2 = va_arg(ap, const char *);
+//         while (*s2) {
+//           out[pos++] = *s2;
+//           s2++;
+//         }
+//         longarg = 0;
+//         format = 0;
+//         break;
+//       }
+//       case 'c': {
+//         out[pos++] = (char)va_arg(ap, int);
+//         longarg = 0;
+//         format = 0;
+//         break;
+//       }
+//       default:
+//         break;
+//       }
+//     } else if (*fmt == '%') {
+//       format = 1;
+//     } else {
+//       out[pos++] = *fmt;
+//     }
+//   }
+//   out[pos++] = '\0';
+//   return pos;
+// }
+
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  int format = 0;
-  int longarg = 0;
-  size_t pos = 0;
-  for (; *fmt; fmt++) {
-    if (format) {
-      switch (*fmt) {
-      case '%': {
-        out[pos++] = '%';
-        longarg = 0;
-        format = 0;
-        break;
-      }
-      case 'l': {
-        ++longarg;
-        break;
-      }
-      case 'p': {
-        ++longarg;
-        out[pos++] = '0';
-        out[pos++] = 'x';
-      }
-      case 'x': {
-        long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
-        int hexdigits = 2 * (longarg ? sizeof(long) : sizeof(int)) - 1;
-        for (int i = hexdigits; i >= 0; i--) {
-          int d = (num >> (4 * i)) & 0xF;
-          out[pos] = (d < 10 ? '0' + d : 'a' + d - 10);
-          pos++;
-        }
-        longarg = 0;
-        format = 0;
-        break;
-      }
-      case 'd': {
-        long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
-        if (num < 0) {
-          num = -num;
-          out[pos] = '-';
-          pos++;
-        }
-        long digits = 1;
-        for (long nn = num; nn /= 10; digits++)
-          ;
-        for (int i = digits - 1; i >= 0; i--) {
-          out[pos + i] = '0' + (num % 10);
-          num /= 10;
-        }
-        pos += digits;
-        longarg = 0;
-        format = 0;
-        break;
-      }
-      case 's': {
-        const char *s2 = va_arg(ap, const char *);
-        while (*s2) {
-          out[pos++] = *s2;
-          s2++;
-        }
-        longarg = 0;
-        format = 0;
-        break;
-      }
-      case 'c': {
-        out[pos++] = (char)va_arg(ap, int);
-        longarg = 0;
-        format = 0;
-        break;
-      }
-      default:
-        break;
-      }
-    } else if (*fmt == '%') {
-      format = 1;
-    } else {
-      out[pos++] = *fmt;
-    }
-  }
-  out[pos++] = '\0';
-  return pos;
+  int res = 0;
+  res = vsnprintf(out, -1, fmt, ap);
+  return res;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
@@ -115,12 +124,6 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   va_end(ap);
   return res;
 }
-
-/*
-  %d
-  %s
-  %c
- */
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   int format = 0;
