84c56936c9238dbcfc2dfb3d519e7540fd31405a
tracer-ysyx <tracer@ysyx.org>
2024-09-16 15:49:24 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:49:24 up 15:58,  1 user,  load average: 0.30, 0.32, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -10,8 +10,8 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t rtc_reg1 = inl(RTC_ADDR);
   uint32_t rtc_reg2 = inl(RTC_ADDR + 0x0000004);
 
-  uptime->us = ((uint64_t)rtc_reg2 << 32) | (uint64_t)rtc_reg1;
-  printf("haha %d\n", uptime->us);
+  uptime->us = ((uint64_t)rtc_reg1 << 32) | (uint64_t)rtc_reg2;
+  // printf("haha %d\n", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
