2cf8e86baf5064ebe457f55bddee669e8babef83
tracer-ysyx <tracer@ysyx.org>
2024-09-18 15:37:33 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:37:33 up  1:41,  1 user,  load average: 0.17, 0.19, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t input = inl(KBD_ADDR);
+  kbd->keycode = input;
+  kbd->keydown = (input & KEYDOWN_MASK) >> 31;
 }
--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -66,8 +66,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
 #ifdef CONFIG_DTRACE_COND
-  // printf("[Timestamp: %s]\n", timestamp);
-  Log("Device: %s (Address Space: "FMT_PADDR" - "FMT_PADDR")\n", map->name,
+  Log("Device: %s (Address Space: "FMT_PADDR" - "FMT_PADDR")", map->name,
          map->low, map->high);
   Log("Action: READ");
   Log("Accessed Address: " FMT_PADDR, addr);
@@ -79,9 +78,14 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
-#ifdef CONFIG_DTRACE_COND
-#endif
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+#ifdef CONFIG_DTRACE_COND
+  Log("Device: %s (Address Space: "FMT_PADDR" - "FMT_PADDR")", map->name,
+         map->low, map->high);
+  Log("Action: WRITE");
+  Log("Accessed Address: " FMT_PADDR, addr);
+  Log("Write Data: "FMT_WORD"\n", data);
+#endif
 }
