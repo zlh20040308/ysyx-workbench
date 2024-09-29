5fbb41c0c99beb16dbf80e5c7e4ea558dcd8a807
tracer-ysyx <tracer@ysyx.org>
2024-09-29 20:16:04 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  20:16:04 up  1:57,  2 users,  load average: 0.73, 0.51, 0.41

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -18,8 +18,13 @@
 
 #include <common.h>
 
+#define MTVEC 0x305
+#define MSTATUS 0x300
+#define MEPC 0x341
+#define MCAUSE 0x342
 typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
+  word_t csr[4096];
   vaddr_t pc;
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
