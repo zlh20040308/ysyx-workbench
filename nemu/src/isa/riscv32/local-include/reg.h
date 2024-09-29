392bb75319317e11abf287773a95941553efe825
tracer-ysyx <tracer@ysyx.org>
2024-09-29 20:31:28 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  20:31:28 up  2:13,  2 users,  load average: 1.46, 1.23, 0.80

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -31,6 +31,8 @@ static inline int check_reg_idx(int idx) {
 }
 
 static inline int check_csr_idx(int idx) {
+  Log("check_csr_idx\n");
+
   IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 4096));
   return idx;
 }
