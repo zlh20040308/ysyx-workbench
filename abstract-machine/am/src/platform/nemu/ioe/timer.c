e397b6b081e9c98e7178237a7eea2a1cda7db32b
tracer-ysyx <tracer@ysyx.org>
2024-09-29 15:35:05 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  15:35:05 up 26 min,  2 users,  load average: 1.52, 1.25, 0.92

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -7,7 +7,11 @@
 void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = (uint64_t)inl(RTC_ADDR) | ((uint64_t)inl(RTC_ADDR + 0x0000004) << 32);
+  uptime->us =
+      ((uint64_t)inl(RTC_ADDR + 0x0000004) << 32) | (uint64_t)inl(RTC_ADDR);
+
+  // uptime->us = (uint64_t)inl(RTC_ADDR) | ((uint64_t)inl(RTC_ADDR + 0x0000004)
+  // << 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
