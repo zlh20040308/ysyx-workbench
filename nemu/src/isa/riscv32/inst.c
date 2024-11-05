f60c3985667e9953566d2d8703ecbf3831de78b7
tracer-ysyx <tracer@ysyx.org>
2024-11-05 23:32:31 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  23:32:31 up  5:11,  1 user,  load average: 0.43, 0.25, 0.19

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -291,7 +291,9 @@ static int decode_exec(Decode *s) {
 
   R(0) = 0; // reset $zero to 0
 
-  CSRs(MSTATUS) = (CSRs(MSTATUS) & 0x80207888) | 0x1800;
+//   CSRs(MSTATUS) = (CSRs(MSTATUS) & 0x80207888) | 0x1800;
+  CSRs(MSTATUS) = CSRs(MSTATUS) & 0x80207888;
+
 
 #ifdef CONFIG_FTRACE_COND
   const char *funct_name = "???";
