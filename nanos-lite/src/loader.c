be154f2b4463d0798480a456ce6e98ec99386f55
tracer-ysyx <tracer@ysyx.org>
2024-11-06 16:11:53 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  16:11:53 up  4:28,  1 user,  load average: 0.53, 0.34, 0.37

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,7 +9,6 @@
 #define Elf_Phdr Elf32_Phdr
 #endif
 
-
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern void init_ramdisk();
@@ -23,20 +22,33 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   assert(elf_header.e_ident[1] == 'E');
   assert(elf_header.e_ident[2] == 'L');
   assert(elf_header.e_ident[3] == 'F');
-
+  Log("elf_header.e_ident = %s", elf_header.e_ident);
   // 遍历程序头表
+  Log("elf_header.e_phnum = %x", elf_header.e_phnum);
   for (int i = 0; i < elf_header.e_phnum; i++) {
-    Elf32_Phdr phdr;
+    Elf_Phdr phdr;
     size_t ph_offset = elf_header.e_phoff + i * elf_header.e_phentsize;
-    ramdisk_read(&phdr, ph_offset, sizeof(phdr));
+    Log("ph_offset = %x", ph_offset);
+    Log("elf_header.e_phoff = %x", elf_header.e_phoff);
+    Log("elf_header.e_phentsize = %x", elf_header.e_phentsize);
+    Log("sizeof(phdr) = %x", sizeof(phdr));
 
-    if (phdr.p_type == 1) { // PT_LOAD 段
+    ramdisk_read(&phdr, ph_offset, sizeof(phdr));
+    Log("phdr.p_type = %x", phdr.p_type);
+    Log("phdr.p_vaddr = %x", phdr.p_vaddr);
+    Log("phdr.p_filesz = %x", phdr.p_filesz);
+
+    
+
+    if (phdr.p_type == PT_LOAD) { // PT_LOAD 段
+      Log("IS LOAD\n");
       // 将该段加载到指定内存位置
-      ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+      ramdisk_read((void *)(uintptr_t)phdr.p_vaddr, phdr.p_offset,
+                   phdr.p_filesz);
 
       // 如果内存段比文件中的数据大，则清零剩余部分
       if (phdr.p_memsz > phdr.p_filesz) {
-        memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0,
+        memset((void *)(uintptr_t)(phdr.p_vaddr + phdr.p_filesz), 0,
                phdr.p_memsz - phdr.p_filesz);
       }
     }
