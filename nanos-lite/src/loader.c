0ab5a334326a25e42005573b8eb9faa9d91f0120
tracer-ysyx <tracer@ysyx.org>
2024-11-06 20:27:59 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  20:27:59 up  5:58,  1 user,  load average: 0.53, 0.45, 0.32

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,10 +4,14 @@
 #ifdef __LP64__
 #define Elf_Ehdr Elf64_Ehdr
 #define Elf_Phdr Elf64_Phdr
+#define HAHAHA 10000
 #else
 #define Elf_Ehdr Elf32_Ehdr
 #define Elf_Phdr Elf32_Phdr
+#define HAHAHA 0
 #endif
+
+extern uint8_t* ramdisk_start;
 
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
@@ -15,47 +19,51 @@ extern void init_ramdisk();
 extern size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr elf_header;
-  ramdisk_read(&elf_header, 0, sizeof(elf_header));
+  Log("%d", HAHAHA);
+  const Elf_Ehdr* elf_header = (Elf_Ehdr*)&ramdisk_start;
+
+
   // 使用 assert 对 ELF 文件的魔数和其他字段进行检查
-  assert(elf_header.e_ident[0] == 0x7F);
-  assert(elf_header.e_ident[1] == 'E');
-  assert(elf_header.e_ident[2] == 'L');
-  assert(elf_header.e_ident[3] == 'F');
-  Log("elf_header.e_ident = %s", elf_header.e_ident);
+  assert(elf_header->e_ident[0] == 0x7F);
+  assert(elf_header->e_ident[1] == 'E');
+  assert(elf_header->e_ident[2] == 'L');
+  assert(elf_header->e_ident[3] == 'F');
+  Log("elf_header.e_ident = %s", elf_header->e_ident);
   // 遍历程序头表
-  Log("elf_header.e_phnum = %x", elf_header.e_phnum);
-  for (int i = 0; i < elf_header.e_phnum; i++) {
-    Elf_Phdr phdr;
-    size_t ph_offset = elf_header.e_phoff + i * elf_header.e_phentsize;
-    Log("ph_offset = %x", ph_offset);
-    Log("elf_header.e_phoff = %x", elf_header.e_phoff);
-    Log("elf_header.e_phentsize = %x", elf_header.e_phentsize);
-    Log("sizeof(phdr) = %x", sizeof(phdr));
+  Log("elf_header.e_phnum = %x", elf_header->e_phnum);
+  Log("elf_header.e_phoff = %d", elf_header->e_phoff);
+  Log("elf_header.e_shoff = %d", elf_header->e_shoff);
+  Log("elf_header.e_phentsize = %x", elf_header->e_phentsize);
+  // printf("ELF loaded, entry point at: %x\n", elf_header.e_entry);
 
-    ramdisk_read(&phdr, ph_offset, sizeof(phdr));
-    Log("phdr.p_type = %x", phdr.p_type);
-    Log("phdr.p_vaddr = %x", phdr.p_vaddr);
-    Log("phdr.p_filesz = %x", phdr.p_filesz);
+  // for (int i = 0; i < elf_header.e_phnum; i++) {
+  //   Elf_Phdr phdr;
+  //   size_t ph_offset = elf_header.e_phoff + i * elf_header.e_phentsize;
+  //   Log("ph_offset = %x", ph_offset);
 
-    
+  //   Log("sizeof(phdr) = %x", sizeof(phdr));
 
-    if (phdr.p_type == PT_LOAD) { // PT_LOAD 段
-      Log("IS LOAD\n");
-      // 将该段加载到指定内存位置
-      ramdisk_read((void *)(uintptr_t)phdr.p_vaddr, phdr.p_offset,
-                   phdr.p_filesz);
+  //   ramdisk_read(&phdr, ph_offset, sizeof(phdr));
+  //   Log("phdr.p_type = %x", phdr.p_type);
+  //   Log("phdr.p_vaddr = %x", phdr.p_vaddr);
+  //   Log("phdr.p_filesz = %x", phdr.p_filesz);
 
-      // 如果内存段比文件中的数据大，则清零剩余部分
-      if (phdr.p_memsz > phdr.p_filesz) {
-        memset((void *)(uintptr_t)(phdr.p_vaddr + phdr.p_filesz), 0,
-               phdr.p_memsz - phdr.p_filesz);
-      }
-    }
-  }
+  //   if (phdr.p_type == PT_LOAD) { // PT_LOAD 段
+  //     Log("IS LOAD\n");
+  //     // 将该段加载到指定内存位置
+  //     ramdisk_read((void *)(uintptr_t)phdr.p_vaddr, phdr.p_offset,
+  //                  phdr.p_filesz);
 
-  printf("ELF loaded, entry point at: %p\n", (void *)elf_header.e_entry);
-  return (uintptr_t)elf_header.e_entry;
+  //     // 如果内存段比文件中的数据大，则清零剩余部分
+  //     if (phdr.p_memsz > phdr.p_filesz) {
+  //       memset((void *)(uintptr_t)(phdr.p_vaddr + phdr.p_filesz), 0,
+  //              phdr.p_memsz - phdr.p_filesz);
+  //     }
+  //   }
+  // }
+
+  printf("ELF loaded, entry point at: %x\n", elf_header->e_entry);
+  return (uintptr_t)elf_header->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
