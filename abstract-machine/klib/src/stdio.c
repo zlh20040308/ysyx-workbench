f0f3da4331b4087e81bcf4e13468280d36639391
tracer-ysyx <tracer@ysyx.org>
2024-11-06 21:03:04 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  21:03:05 up  6:33,  1 user,  load average: 0.07, 0.10, 0.15

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,22 +8,27 @@
 #define BUFFER_SIZE 4096
 static char buf[BUFFER_SIZE];
 
-// Helper function to convert an integer to hexadecimal string
-void itohex(uintptr_t value, char *buffer, size_t buffer_size) {
+static void write_hex(char *out, size_t *pos, size_t n, uintptr_t val,
+                      size_t *count) {
   const char *hex_digits = "0123456789abcdef";
-  int index = buffer_size - 1;
-  buffer[index--] = '\0';
+  char buffer[16];
+  int i = 0;
 
-  // Convert the value to hex and store it in reverse order
-  do {
-    buffer[index--] = hex_digits[value & 0xf];
-    value >>= 4;
-  } while (value != 0 && index > 0);
+  if (val == 0) {
+    if (*pos < n)
+      out[(*pos)++] = '0';
+    (*count)++;
+    return;
+  }
 
-  // If there's room, prepend "0x"
-  if (index >= 1) {
-    buffer[index--] = 'x';
-    buffer[index] = '0';
+  while (val) {
+    buffer[i++] = hex_digits[val % 16];
+    val /= 16;
+  }
+  while (i--) {
+    if (*pos < n)
+      out[(*pos)++] = buffer[i];
+    (*count)++;
   }
 }
 
@@ -35,88 +40,6 @@ int printf(const char *fmt, ...) {
   putstr(buf);
   return ret;
 }
-
-// int vsprintf(char *out, const char *fmt, va_list ap) {
-//   int format = 0;
-//   int longarg = 0;
-//   size_t pos = 0;
-//   for (; *fmt; fmt++) {
-//     if (format) {
-//       switch (*fmt) {
-//       case '%': {
-//         out[pos++] = '%';
-//         longarg = 0;
-//         format = 0;
-//         break;
-//       }
-//       case 'l': {
-//         ++longarg;
-//         break;
-//       }
-//       case 'p': {
-//         ++longarg;
-//         out[pos++] = '0';
-//         out[pos++] = 'x';
-//       }
-//       case 'x': {
-//         long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
-//         int hex_digits = 2 * (longarg ? sizeof(long) : sizeof(int)) - 1;
-//         for (int i = hex_digits; i >= 0; i--) {
-//           int d = (num >> (4 * i)) & 0xF;
-//           out[pos] = (d < 10 ? '0' + d : 'a' + d - 10);
-//           pos++;
-//         }
-//         longarg = 0;
-//         format = 0;
-//         break;
-//       }
-//       case 'd': {
-//         long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
-//         if (num < 0) {
-//           num = -num;
-//           out[pos] = '-';
-//           pos++;
-//         }
-//         long digits = 1;
-//         for (long nn = num; nn /= 10; digits++)
-//           ;
-//         for (int i = digits - 1; i >= 0; i--) {
-//           out[pos + i] = '0' + (num % 10);
-//           num /= 10;
-//         }
-//         pos += digits;
-//         longarg = 0;
-//         format = 0;
-//         break;
-//       }
-//       case 's': {
-//         const char *s2 = va_arg(ap, const char *);
-//         while (*s2) {
-//           out[pos++] = *s2;
-//           s2++;
-//         }
-//         longarg = 0;
-//         format = 0;
-//         break;
-//       }
-//       case 'c': {
-//         out[pos++] = (char)va_arg(ap, int);
-//         longarg = 0;
-//         format = 0;
-//         break;
-//       }
-//       default:
-//         break;
-//       }
-//     } else if (*fmt == '%') {
-//       format = 1;
-//     } else {
-//       out[pos++] = *fmt;
-//     }
-//   }
-//   out[pos++] = '\0';
-//   return pos;
-// }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   int res = 0;
@@ -192,16 +115,14 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
         format = 0;
         break;
       }
-      case 'p': { // New case for pointer
-        void *ptr = va_arg(ap, void *);
-        char ptr_str[2 * sizeof(void *) * 2 +
-                    3]; // Enough space for "0x" + hex + null
-        itohex((uintptr_t)ptr, ptr_str, sizeof(ptr_str));
-        size_t len = strlen(ptr_str);
-        for (size_t i = 0; i < len && pos < n; i++) {
-          out[pos++] = ptr_str[i];
-        }
-        count += len;
+      case 'p': { // Handle %p
+        uintptr_t ptr_val = (uintptr_t)va_arg(ap, void *);
+        if (pos < n)
+          out[pos++] = '0';
+        if (pos < n)
+          out[pos++] = 'x';
+        count += 2;
+        write_hex(out, &pos, n, ptr_val, &count);
         format = 0;
         break;
       }
