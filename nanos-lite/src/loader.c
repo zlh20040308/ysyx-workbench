54f7f688e4c250fbdd70402c03cd9f1924e53a4a
tracer-ysyx <tracer@ysyx.org>
2024-11-06 20:57:22 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  20:57:22 up  6:27,  1 user,  load average: 0.19, 0.19, 0.20

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -26,31 +26,14 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   assert(elf_header.e_ident[1] == 'E');
   assert(elf_header.e_ident[2] == 'L');
   assert(elf_header.e_ident[3] == 'F');
-  Log("elf_header.e_ident = %s", elf_header.e_ident);
-  Log("elf_header.e_phnum = %x", elf_header.e_phnum);
-  Log("elf_header.e_phoff = %d", elf_header.e_phoff);
-  Log("elf_header.e_shoff = %d", elf_header.e_shoff);
-  Log("elf_header.e_phentsize = %x", elf_header.e_phentsize);
-
   for (int i = 0; i < elf_header.e_phnum; i++) {
     Elf_Phdr phdr;
     size_t ph_offset = elf_header.e_phoff + i * elf_header.e_phentsize;
-    Log("ph_offset = %x", ph_offset);
-
-    Log("sizeof(phdr) = %x", sizeof(phdr));
-
     ramdisk_read(&phdr, ph_offset, sizeof(phdr));
-    Log("phdr.p_type = %x", phdr.p_type);
-    Log("phdr.p_vaddr = %x", phdr.p_vaddr);
-    Log("phdr.p_filesz = %x", phdr.p_filesz);
-
     if (phdr.p_type == PT_LOAD) { // PT_LOAD 段
-      Log("IS LOAD\n");
       // 将该段加载到指定内存位置
       ramdisk_read((void *)(uintptr_t)phdr.p_vaddr, phdr.p_offset,
                    phdr.p_filesz);
-      Log("IS LOAD\n");
-
       // 如果内存段比文件中的数据大，则清零剩余部分
       if (phdr.p_memsz > phdr.p_filesz) {
         memset((void *)(uintptr_t)(phdr.p_vaddr + phdr.p_filesz), 0,
--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -12,10 +12,7 @@ extern uint8_t ramdisk_end;
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
-  Log("ahah");
   memcpy(buf, &ramdisk_start + offset, len);
-  Log("ahah");
-
   return len;
 }
 
