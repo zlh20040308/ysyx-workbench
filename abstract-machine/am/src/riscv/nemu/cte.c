018db4592cf3c07690d64cdbeecf6214ecc4a737
tracer-ysyx <tracer@ysyx.org>
2024-11-09 09:51:47 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:51:47 up  4:14,  1 user,  load average: 1.69, 0.67, 0.42

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -16,7 +16,7 @@ Context *__am_irq_handle(Context *c) {
       ev.event = EVENT_YIELD;
       break;
     default:
-      printf("ahhahahaaaaaaaaaaaaaaa\n");
+      printf("c->gpr[17] = %d\n",c->gpr[17]);
       ev.event = EVENT_ERROR;
       break;
     }
