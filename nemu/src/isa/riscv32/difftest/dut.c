adbe3f3e672f1a7e4114d685c6e4dccbc94b65b1
tracer-ysyx <tracer@ysyx.org>
2024-11-14 15:23:29 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  15:23:29 up 42 min,  2 users,  load average: 0.80, 1.04, 0.90

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -35,18 +35,18 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if (!difftest_check_csr(csr_name(MEPC), pc, ref_r->sr[MEPC], sr(MEPC))) {
     success = false;
   }
-  if (!difftest_check_csr(csr_name(MSTATUS), pc, ref_r->sr[MSTATUS],
-                          sr(MSTATUS))) {
-    success = false;
-  }
+  // if (!difftest_check_csr(csr_name(MSTATUS), pc, ref_r->sr[MSTATUS],
+  //                         sr(MSTATUS))) {
+  //   success = false;
+  // }
   if (!difftest_check_csr(csr_name(MTVEC), pc, ref_r->sr[MTVEC], sr(MTVEC))) {
     success = false;
   }
 
-  // if (!difftest_check_csr(csr_name(MCAUSE), pc, ref_r->sr[MCAUSE],
-  //                         sr(MCAUSE))) {
-  //   success = false;
-  // }
+  if (!difftest_check_csr(csr_name(MCAUSE), pc, ref_r->sr[MCAUSE],
+                          sr(MCAUSE))) {
+    success = false;
+  }
 
   return success;
 }
