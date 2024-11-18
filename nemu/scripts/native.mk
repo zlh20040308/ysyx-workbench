7aa96fedf95a6b462137049e51e4bea97e3f3356
tracer-ysyx <tracer@ysyx.org>
2024-11-18 22:01:09 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.11.8-arch1-2 #1 SMP PREEMPT_DYNAMIC Fri, 15 Nov 2024 15:35:07 +0000 x86_64 GNU/Linux  22:01:09 up 12:46,  2 users,  load average: 1.06, 0.88, 0.79

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -42,7 +42,9 @@ run: run-env
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
-	gdb -s $(BINARY) --args $(NEMU_EXEC)
+	# gdb -s $(BINARY) --args $(NEMU_EXEC)
+	gdb --args $(BINARY) $(NEMU_EXEC)
+
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
