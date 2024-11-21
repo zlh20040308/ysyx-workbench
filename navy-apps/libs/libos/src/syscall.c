b4dc59335b49201d713e289ab090e06af8ed1fe9
tracer-ysyx <tracer@ysyx.org>
2024-11-22 00:23:53 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  00:23:53 up  2:03,  1 user,  load average: 0.13, 0.17, 0.14

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -75,7 +75,7 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  return _syscall_(SYS_brk, increment, 0, 0);
+  return (void *)_syscall_(SYS_brk, increment, 0, 0);
 }
 
 
