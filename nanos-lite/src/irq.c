0e8202cd69560654c60d66a012e0cbc47e4dfaaa
tracer-ysyx <tracer@ysyx.org>
2024-11-21 21:57:30 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.11.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 17 Nov 2024 16:06:17 +0000 x86_64 GNU/Linux  21:57:30 up  9:48,  2 users,  load average: 0.88, 1.29, 1.29

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,19 +1,8 @@
 #include <common.h>
-#include "syscall.h"
-static Context *do_event(Event e, Context *c) {
+
+static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-  case EVENT_YIELD:
-    printf("You son of bitch\n");
-    break;
-  case EVENT_SYSCALL:
-    do_syscall(c);
-    break;
-  case EVENT_ERROR:
-    panic("Ops, something go wrong!");
-    break;
-  default:
-  Log("haha = %d",EVENT_YIELD);
-    panic("Unhandled event ID = %d", e.event);
+    default: panic("Unhandled event ID = %d", e.event);
   }
 
   return c;
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,53 +1,22 @@
-#include <elf.h>
 #include <proc.h>
+#include <elf.h>
 
 #ifdef __LP64__
-#define Elf_Ehdr Elf64_Ehdr
-#define Elf_Phdr Elf64_Phdr
-#define HAHAHA 10000
+# define Elf_Ehdr Elf64_Ehdr
+# define Elf_Phdr Elf64_Phdr
 #else
-#define Elf_Ehdr Elf32_Ehdr
-#define Elf_Phdr Elf32_Phdr
-#define HAHAHA 0
+# define Elf_Ehdr Elf32_Ehdr
+# define Elf_Phdr Elf32_Phdr
 #endif
 
-extern uint8_t *ramdisk_start;
-
-extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-extern void init_ramdisk();
-extern size_t get_ramdisk_size();
-
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr elf_header;
-  ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
-  // 使用 assert 对 ELF 文件的魔数和其他字段进行检查
-  assert(elf_header.e_ident[0] == 0x7F);
-  assert(elf_header.e_ident[1] == 'E');
-  assert(elf_header.e_ident[2] == 'L');
-  assert(elf_header.e_ident[3] == 'F');
-  for (int i = 0; i < elf_header.e_phnum; i++) {
-    Elf_Phdr phdr;
-    size_t ph_offset = elf_header.e_phoff + i * elf_header.e_phentsize;
-    ramdisk_read(&phdr, ph_offset, sizeof(phdr));
-    if (phdr.p_type == PT_LOAD) { // PT_LOAD 段
-      // 将该段加载到指定内存位置
-      ramdisk_read((void *)(uintptr_t)phdr.p_vaddr, phdr.p_offset,
-                   phdr.p_filesz);
-      // 如果内存段比文件中的数据大，则清零剩余部分
-      if (phdr.p_memsz > phdr.p_filesz) {
-        memset((void *)(uintptr_t)(phdr.p_vaddr + phdr.p_filesz), 0,
-               phdr.p_memsz - phdr.p_filesz);
-      }
-    }
-  }
-
-  printf("ELF loaded, entry point at: %x\n", elf_header.e_entry);
-  return (uintptr_t)elf_header.e_entry;
+  TODO();
+  return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  ((void (*)())entry)();
+  ((void(*)())entry) ();
 }
+
