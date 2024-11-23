e454fd003fdb85c37edd86a4dd9695b4373a7233
tracer-ysyx <tracer@ysyx.org>
2024-11-23 16:26:07 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 5.15.153.1-microsoft-standard-WSL2+ #1 SMP Fri May 10 11:23:37 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  16:26:07 up  5:47,  1 user,  load average: 0.44, 0.39, 0.39

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -124,10 +124,10 @@ size_t fs_lseek(int fd, size_t offset, int whence) {
   case SEEK_END:
     Log("file_table[fd].size + offset - 1 = %d", file_table[fd].size + offset - 1);
     Log("file_table[fd].size = %d", file_table[fd].size);
-    if (file_table[fd].size + offset - 1 >= file_table[fd].size) {
+    if (file_table[fd].size + offset >= file_table[fd].size) {
       return -1; // 溢出或超出文件范围
     }
-    new_offset = file_table[fd].size + offset - 1;
+    new_offset = file_table[fd].size + offset;
     break;
   default:
     return -1; // 无效的 whence 参数
