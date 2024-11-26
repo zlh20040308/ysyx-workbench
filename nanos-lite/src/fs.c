47096ffa655901f08bed4394d9463731a897392d
tracer-ysyx <tracer@ysyx.org>
2024-11-27 01:15:34 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.11.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 17 Nov 2024 16:06:17 +0000 x86_64 GNU/Linux  01:15:34 up  1:43,  2 users,  load average: 0.81, 0.92, 0.81

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -20,7 +20,7 @@ typedef struct {
   size_t open_offset;
 } Finfo;
 
-enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS };
+enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENTS, FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
