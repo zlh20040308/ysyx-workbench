950acc48c7a4c7cefcbd513a47fa7675337ee2e7
tracer-ysyx <tracer@ysyx.org>
2024-11-27 13:45:10 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  13:45:10 up 14 min,  2 users,  load average: 1.40, 1.54, 1.07

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -58,7 +58,9 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return sprintf(buf, "WIDTH :%d\nHEIGHT:%d\n", screen_w, screen_h);
+  int p = sprintf(buf, "WIDTH :%d\nHEIGHT:%d\n", screen_w, screen_h);
+  Log("%s", buf);
+  return p;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) { return 0; }
