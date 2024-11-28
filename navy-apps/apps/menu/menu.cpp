78cadabec081d463076cb631eab7593a6e51bf44
tracer-ysyx <tracer@ysyx.org>
2024-11-28 21:40:06 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  21:40:06 up 11:43,  2 users,  load average: 0.55, 0.72, 0.79

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -74,6 +74,9 @@ int main(int argc, char *argv[], char *envp[]) {
   font = new BDF_Font(font_fname);
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
   assert(logo_sf);
+  while (1) {
+  
+  }
   set_i_max();
 
   while (1) {
