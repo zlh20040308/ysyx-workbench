4da59e614b7ed2a41e8a084eb7a398d12012917b
tracer-ysyx <tracer@ysyx.org>
2024-11-29 11:50:47 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  11:50:47 up 18 min,  2 users,  load average: 2.47, 1.85, 1.18

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -84,7 +84,6 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fbdev = open("/dev/fb", O_WRONLY);
   // printf("canvas_x = %d, canvas_y = %d\n", canvas_x, canvas_y);
   printf("x = %d, y = %d, w = %d, h = %d\n", x, y, w, h);
-  w/=2;
   for (int i = 0; i < h/2; ++i) {
     size_t offset =
         ((x + canvas_x) + (y + canvas_y + i) * screen_w) * sizeof(uint32_t);
