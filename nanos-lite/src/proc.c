cbe9abcb259a8aa607799565aed579eed1be2c05
tracer-ysyx <tracer@ysyx.org>
2024-11-29 14:47:43 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  14:47:43 up  3:15,  2 users,  load average: 1.68, 1.76, 1.48

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -34,7 +34,7 @@ void init_proc() {
   // naive_uload(NULL, "/bin/nslider");
   // naive_uload(NULL, "/bin/menu");
   // naive_uload(NULL, "/bin/nterm");
-  naive_uload(NULL, "bmp-test");
+  naive_uload(NULL, "/bin/bmp-test");
 }
 
 Context *schedule(Context *prev) { return NULL; }
