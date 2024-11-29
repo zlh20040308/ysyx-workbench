a82b70291cade9fe7f4f58d7537f658052f2d5da
tracer-ysyx <tracer@ysyx.org>
2024-11-29 14:37:25 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  14:37:25 up  3:05,  2 users,  load average: 1.23, 1.32, 1.21

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -64,7 +64,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   // Calculate the starting point (x1, y1) and ending point (x2, y2)
   // Log("offset = 0x%8x, len = 0x%8x", offset, len);
-  size_t screen_w_real = screen_w * sizeof(uint32_t);
+  size_t screen_w_real = screen_w;
 
   size_t x1 = offset % screen_w_real;
   size_t y1 = offset / screen_w_real;
