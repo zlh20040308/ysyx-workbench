2f5865a3c2d4ab845c38b146469393bf0b71f013
tracer-ysyx <tracer@ysyx.org>
2024-11-29 14:43:46 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  14:43:46 up  3:11,  2 users,  load average: 2.41, 1.63, 1.38

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -82,16 +82,16 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   // 打开 /dev/fb 文件
   int fbdev = open("/dev/fb", O_WRONLY);
-  printf("screen_w = %d, screen_h = %d\n", screen_w, screen_h);
-  printf("canvas_x = %d, canvas_y = %d\n", canvas_x, canvas_y);
-  printf("x = %d, y = %d, w = %d, h = %d\n", x, y, w, h);
+  // printf("screen_w = %d, screen_h = %d\n", screen_w, screen_h);
+  // printf("canvas_x = %d, canvas_y = %d\n", canvas_x, canvas_y);
+  // printf("x = %d, y = %d, w = %d, h = %d\n", x, y, w, h);
   for (int i = 0; i < h; ++i) {
     size_t offset =
         ((x + canvas_x) + (y + canvas_y + i) * screen_w) * sizeof(uint32_t);
 
     lseek(fbdev, offset, SEEK_SET);
 
-    printf("offset = %d, n = %d\n", offset, w * sizeof(uint32_t));
+    // printf("offset = %d, n = %d\n", offset, w * sizeof(uint32_t));
     
     write(fbdev, pixels + i * w, w * sizeof(uint32_t));
   }
