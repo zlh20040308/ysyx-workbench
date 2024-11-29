45847ded2c9264fc84ef5015939df9858be62358
tracer-ysyx <tracer@ysyx.org>
2024-11-29 14:22:08 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  14:22:08 up  2:49,  2 users,  load average: 1.91, 1.00, 1.01

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -81,7 +81,9 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   if (len <= first_row_len) {
     // If len is less than or equal to screen_w - x1, only write the first
     // segment
-    io_write(AM_GPU_FBDRAW, x1, y1, buf, len / sizeof(uint32_t), 1, false);
+    while (1) {
+    }
+    io_write(AM_GPU_FBDRAW, x1, y1, buf, len / sizeof(uint32_t), 1, true);
     Log("x1 = %d, y1 = %d, len / sizeof(uint32_t) = %d", x1, y1,
         len / sizeof(uint32_t));
   } else {
@@ -105,8 +107,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
              last_row_len / sizeof(uint32_t), 1, true);
   }
   Log("offset = %d, len = %d\n", offset, len);
-  while (1) {
-  }
+
   return len;
 }
 
