02962f440c8b0499784e6aad55c98e44d3736a76
tracer-ysyx <tracer@ysyx.org>
2024-11-29 21:33:06 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  21:33:06 up 10:00,  2 users,  load average: 0.34, 0.58, 0.63

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -74,7 +74,7 @@ void process_file(const char *filename)
 }
 
 int main(int argc, char *argv[]) {
-  Log("argv[0] = %s", argv[0]);
+  printf("argv[0] = %s\n", argv[0]);
   printf("argv[1] = %s\n", argv[1]);
 
   /* Initialize the monitor. */
