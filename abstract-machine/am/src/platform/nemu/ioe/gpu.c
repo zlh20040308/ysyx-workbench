2035131846099ab8475db1d227016df61b947217
tracer-ysyx <tracer@ysyx.org>
2024-11-29 14:28:19 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  14:28:19 up  2:56,  2 users,  load average: 1.53, 1.26, 1.12

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -33,7 +33,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t width = inl(VGACTL_ADDR) >> 16;
   volatile uint32_t *fb = (volatile uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *from = (uint32_t *)(uintptr_t)(ctl->pixels);
-  printf("h = %d, w = %d\n", ctl->h, ctl->w);
+  printf("x = %d, y = %d, h = %d, w = %d\n", ctl->x, ctl->y ,ctl->h, ctl->w);
 
   for (int i = 0; i < ctl->h; i++) {
     for (int j = 0; j < ctl->w; j++) {
