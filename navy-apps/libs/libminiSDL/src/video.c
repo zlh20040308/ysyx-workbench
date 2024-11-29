9e133b47ad853cf2e42ef550c07e8fc186a3417f
tracer-ysyx <tracer@ysyx.org>
2024-11-29 10:26:09 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  10:26:09 up 39 min,  2 users,  load average: 0.48, 0.87, 0.72

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -124,6 +124,9 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     h = screen_h;
   }
   printf("BytesPerPixel = %d", s->format->BytesPerPixel);
+                printf("========================\n");
+  while (1) {
+  }
   // switch (dst->format->BytesPerPixel) {
   // case 1: // 8-bit paletted mode
   //   break;
