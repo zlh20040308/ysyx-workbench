cfc9d031c5e0241b72eb5ac49ca0628440b4ef86
tracer-ysyx <tracer@ysyx.org>
2024-11-30 19:42:46 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  19:42:46 up  9:34,  2 users,  load average: 0.84, 0.84, 1.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -101,6 +101,7 @@ void device_update();
 
 extern bool traverse_watchpoints_and_show_changes();
 
+
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND)
@@ -113,7 +114,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   {
     IFDEF(CONFIG_ITRACE, puts(_this->logbuf));
   }
-  Log("_this->pc = %x, dnpc = %x", _this->pc, dnpc);
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT
   bool is_change = traverse_watchpoints_and_show_changes();
