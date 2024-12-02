cd2c286901c884debbb55099ab1d2514a2d6cc45
tracer-ysyx <tracer@ysyx.org>
2024-12-02 17:12:21 +0800

    > compile NEMU ysyx_23060337 张凌豪 Linux Feng 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  17:12:21 up  5:27,  2 users,  load average: 0.96, 1.14, 1.04

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -116,7 +116,7 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
   case SYS_exit:
-    naive_uload(NULL, "/bin/menu");
+    naive_uload(NULL, "/bin/nterm");
     halt(c->GPR2);
     break;
   case SYS_yield:
